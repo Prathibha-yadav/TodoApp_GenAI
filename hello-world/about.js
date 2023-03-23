e47a5875401e02d0a284521ea73ab050d752ac1f
prathibha <prathibha@LAPTOP-NAAMUCTV>
2023-03-23 21:38:11 +0530

    Initial commit

---
+++ hello-world/about.js
@@ -0,0 +1,13 @@
+/*const readline = require("readline");
+
+const lineDetail = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+lineDetail.question(`Please provide your name - `, (name) => {
+  console.log(`Hi ${name}!`);
+  lineDetail.close();
+});
+*/
+
--- hello-world/index.js
+++
@@ -1,6 +0,0 @@
-const hello = "Hello GitHub!";
-const func = () => {
-  console.log(hello);
-};
-
-func();
