70537c283b84ce2241a06f233c441f70f796c5b8
prathibha <prathibha@LAPTOP-NAAMUCTV>
2024-02-25 20:43:20 +0530

    dockerized application

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,4 +1,5 @@
-const app = require('./app')
-app.listen(3300, () => {
-  console.log('Started express server at 3300')
-})
+require("dotenv").config();
+const app = require("./app");
+app.listen(process.env.PORT, () => {
+  console.log("Started express server at 3300");
+});
